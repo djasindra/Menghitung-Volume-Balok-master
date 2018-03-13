321b2b246949d0e84115130d30ea39f5ac5ca0f7
Dede Jasindra <djasindra@gmail.com>
2018-03-13 11:12:51 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Button,
+  Text, TextInput,
+  View
+} from 'react-native';
+
+export default class HitungLuasSegitiga extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      panjang:0,
+      tinggi:0,
+      lebar:0,
+      volume:0
+    };
+  }
+
+  render() {
+    return (  
+      <View style = {{flex:1,backgroundColor:'#830303'}}>
+
+        <View style={{backgroundColor:'#2196f3'}}>
+           <Text style = {{padding: 10, fontSize: 20, color: 'white', textAlign:'center'}} >
+            Menghitung luas Segitiga
+          </Text>
+         </View>
+        
+        <View style={{margin:20,padding: 10, backgroundColor:'#e3f2fd'}}>
+            <TextInput style = {{height: 40}}
+              placeholder="Masukkan Panjang"
+              onChangeText={(panjang)=>this.setState({panjang})}
+              keyboardType = 'numeric'
+            />
+            <TextInput style = {{height: 40}}
+              placeholder="Masukkan  Tinggi"
+              onChangeText={(tinggi)=>this.setState({tinggi})}
+              keyboardType = 'numeric'
+            />
+            <TextInput style = {{height: 40}}
+              placeholder="Masukkan  Lebar"
+              onChangeText={(lebar)=>this.setState({lebar})}
+              keyboardType = 'numeric'
+            />
+
+            <Button
+              onPress={()=>this.setState({
+                volume: (this.state.panjang*this.state.tinggi*this.state.lebar)
+              })}
+              title="Hitung"
+              accessibilityLabel="Klik untuk menghitung"
+            />
+       </View>
+
+        <View style={{margin:20, backgroundColor:'#90caf9'}}>
+          <Text style = {{padding: 10, fontSize: 20}} >
+              Panjang =  {this.state.panjang} {"\n"}
+              Tinggi =  {this.state.tinggi} {"\n"}
+              Lebar = {this.state.lebar} {"\n"}
+              Volume = {this.state.volume}
+          </Text>
+         </View>
+      
+   </View> 
+
+    );
+  }
+
+}
+
+
+
+
+
+
+AppRegistry.registerComponent('AppForm2', () => HitungLuasSegitiga);
